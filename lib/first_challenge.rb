f8374f3edf5257a4938fbaa182675315dc5b1843
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-23 20:53:41 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
 contacts.each do |person, data|
   data.each do |attribute, value|
-        attribute.delete_if do |flavor|
+        value.delete_if do |flavor|
           flavor == "strawberry"
 end
 end
