3fa6c47fee88b51d1e8750be9860518073b31962
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-23 20:23:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,10 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if == "strawberry"
+  contacts.each do |key, value|
+  value.delete_if do |a, b| b == "strawberry"
+  end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
